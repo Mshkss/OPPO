23c21d031c916fa7c70dba6f205b01ed6af996e0
tolyan4er322 <Mshkss>
2023-09-09 10:59:57 +0700

    first commit

---
+++ 1/ConsoleApplication228.cpp
@@ -0,0 +1,33 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct point
+{
+	double a, b;
+	string colour;
+};
+
+int main()
+{
+	std::vector<point>points;
+
+	point test1;
+	ifstream in("Text.txt");	
+		if (in.is_open()) {
+			while(!in.eof()){
+				in >> test1.a >> test1.b >> test1.colour;
+				points.push_back(test1);
+
+			}
+	} 
+		//cout << test1.a << " " << test1.b << " " << test1.colour << endl;
+	for (int i = 0; i < points.size(); ++i ) {
+		test1 = points[i];
+		cout << test1.a << " " << test1.b << " " << test1.colour << endl;
+	}
+}
